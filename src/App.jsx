06f2d69e4aef0633777ef688f4984bc458bb6921
jose <joseantoniomaderag@gmail.com>
2024-08-14 15:19:56 -0600

    ajuste login

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route
           path="/dashboard"
